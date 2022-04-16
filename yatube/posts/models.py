7213531771d9acb48edcb16aa7a3523593975c7c
Sasha <sasha95296@mail.ru>
2022-04-16 19:03:40 +0300

    third commit

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -91,9 +91,7 @@ class Follow(models.Model):
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=['user', 'author'],
-                name='unique follow')
-        ]
+                fields=['user', 'author'], name='unique_follow')]
 
     def __str__(self):
         return self.user, self.author
