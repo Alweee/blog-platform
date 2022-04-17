fb70663b0c9cfd4d865b09cea85010249181b5d9
Sasha <sasha95296@mail.ru>
2022-04-17 12:27:41 +0300

    help commit

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'yatube.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db0'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
